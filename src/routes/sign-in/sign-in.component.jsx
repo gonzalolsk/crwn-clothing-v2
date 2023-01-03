6d319596375e60dfbafb910e0495b36b4cb553d4
gonzalolsk <gonzalolsk@gmail.com>
2023-01-03 10:38:49 -0300

    commented google redirection sign in

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,11 +1,28 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+// import { useEffect } from "react";
+// import { getRedirectResult } from "firebase/auth";
+
+import { 
+    // auth,
+    signInWithGooglePopup,
+    createUserDocumentFromAuth,
+    signInWithGoogleRedirect,
+} from "../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
+    // Sign In with Google redirection
+    // useEffect(async () => {
+    //     const response = await getRedirectResult(auth);
+
+    //     if(response){
+    //         const userDocRef = await createUserDocumentFromAuth(response.user);
+    //     }
+    // },[])
     const logGoogleUser = async () => {
         const { user } = await signInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(user);
         
     };
+
 
     return(
     <div>
@@ -13,6 +30,9 @@ const SignIn = () => {
         <button onClick={ logGoogleUser }>
             Sign In with Google Pop Up
         </button>
+        {/* <button onClick={ signInWithGoogleRedirect }>
+            Sign In with Google Redirect
+        </button> */}
     </div>
     )
 }
