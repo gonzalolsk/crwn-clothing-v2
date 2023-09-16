cec6fa198bf4575d01d45a3002f3443ed5d2f99e
gonzalolsk <gonzalolsk@gmail.com>
2023-09-16 12:48:34 -0300

    created checkout page, checkout items, and their functionality

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,15 +2,20 @@ import React from 'react';
 
 import { useContext } from 'react';
 import { CartContext } from '../../contexts/cart.context';
-
+import { useNavigate } from 'react-router-dom';
 import CartItem from '../cart-item/cart-item.component';
 
 import Button from '../button/button.component';
-
 import './cart-dropdown.styles.scss';
 
+
+
 const CartDropdown = () => {
-const { cartItems } = useContext(CartContext);
+  const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
+  const goToCheckoutHandler = () => {
+    return navigate('/checkout');
+  }
 return (
   <div className='cart-dropdown-container'>
     <div className='cart-items'>
@@ -18,7 +23,7 @@ return (
         cartItems.map(item => <CartItem key={item.id} cartItem={item} />)
       }
     </div>
-    <Button>GO TO CHECKOUT</Button>
+      <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
   </div>
 );
 }
