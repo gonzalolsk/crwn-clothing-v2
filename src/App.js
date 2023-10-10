c7c677d70eaeb8c20f0769c2a085170308a76bc0
gonzalolsk <gonzalolsk@gmail.com>
2023-10-10 17:35:31 -0300

    migrate redux to redux-toolkit

--- src/App.js
+++ src/App.js
@@ -12,19 +12,23 @@ import Navigation from './routes/navigation/navigation.component';
 import Authentication from './routes/authentication/authentication.component';
 import Shop from './routes/shop/shop.component';
 import Checkout from './routes/checkout/checkout.component';
-import { setCurrentUser } from './store/user/user.action';
+import { setCurrentUser } from './store/user/user.reducer';
 
 const App = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListener((user) => {
-      if (user) {
+       if (user) {
         createUserDocumentFromAuth(user);
       }
-      dispatch(setCurrentUser(user));
-    });
 
+      const pickedUser = 
+      user && (({ accessToken, email }) => ({ accessToken, email }))(user);
+
+      dispatch(setCurrentUser(pickedUser));
+    });
+    
     return unsubscribe;
   }, []);
 
